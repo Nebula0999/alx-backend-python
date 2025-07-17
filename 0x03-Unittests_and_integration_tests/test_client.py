c51fd7f622f91e944f4146ee7b25b1067002f477
Allan <nabuleallan1@gmail.com>
2025-07-17 12:36:03 +0300

    updated test client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,14 +11,7 @@ from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 
 
-@parameterized_class([
-    {
-        "org_payload": org_payload,
-        "repos_payload": repos_payload,
-        "expected_repos": expected_repos,
-        "apache2_repos": apache2_repos,
-    }
-    ])
+@parameterized_class([{"val": 42}])
 class TestGithubOrgClient(unittest.TestCase):
     """Test case for GithubOrgClient methods"""
 
