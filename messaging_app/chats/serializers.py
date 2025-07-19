5eb5ece160e713afcba8d61c2e9f316c01025cf0
Allan <nabuleallan1@gmail.com>
2025-07-19 11:25:48 +0300

    added serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,17 +1,26 @@
-import models
+from .models import User, Conversation, Message
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
     class Meta:
-        model = 'chats.User'
-        fields = ['user_id', 'username', 'email', 'phone_number', 'first_name', 'last_name', 'role', 'created_at']
-        read_only_fields = ['user_id', 'created_at']
+        model = User
+        fields = ['user_id', 'username', 'email', 'full_name']
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+    def validate_email(self, value):
+        if "spam" in value:
+            raise serializers.ValidationError("Invalid email address.")
+        return value
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
 
     class Meta:
-        model = 'chats.Conversation'
+        model = Conversation
         fields = ['conversation_id', 'participants', 'created_at']
         read_only_fields = ['conversation_id', 'created_at']
 
@@ -20,6 +29,13 @@ class MessageSerializer(serializers.ModelSerializer):
     conversation = serializers.PrimaryKeyRelatedField(queryset='chats.Conversation.objects.all')
 
     class Meta:
-        model = 'chats.Message'
+        model = Message
         fields = ['message_id', 'sender', 'message_body', 'sent_at', 'conversation', 'is_read', 'is_deleted']
         read_only_fields = ['message_id', 'sent_at', 'is_read', 'is_deleted']
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    custom_field = serializers.CharField(required=False, max_length=100)
+
+    class Meta:
+        model = User
+        fields = ['user_id', 'username', 'custom_field']
