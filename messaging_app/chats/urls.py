c69430646f4e5d53402ba65c154cfb594f916789
Allan <nabuleallan1@gmail.com>
2025-07-19 11:39:52 +0300

    added views and urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from .views import UserViewSet, ConversationViewSet, MessageViewSet
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register our viewsets with it
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'conversations', ConversationViewSet)
+router.register(r'messages', MessageViewSet)
 
 urlpatterns = [
-    path('users/', UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
-    path('users/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='user-detail'),
-    path('conversations/', ConversationViewSet.as_view({'get': 'list', 'post': 'create'}), name='conversation-list'),
-    path('conversations/<uuid:pk>/', ConversationViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='conversation-detail'),
-    path('messages/', MessageViewSet.as_view({'get': 'list', 'post': 'create'}), name='message-list'),
-    path('messages/<uuid:pk>/', MessageViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='message-detail'),
+    path('', include(router.urls)),
 ]
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from requests import Response
+from rest_framework import viewsets, status, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import User, Conversation, Message
 from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
 
@@ -7,13 +9,34 @@ from .serializers import UserSerializer, ConversationSerializer, MessageSerializ
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['username', 'email']
+    ordering_fields = ['created_at']
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(
+            {"message": "User created successfully", "data": response.data},
+            status=status.HTTP_201_CREATED
+        )
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['participants']
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['conversation', 'sender']
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(
+            {"message": "Message created successfully", "data": response.data},
+            status=status.HTTP_201_CREATED
+        )
 
 # Create your views here.
