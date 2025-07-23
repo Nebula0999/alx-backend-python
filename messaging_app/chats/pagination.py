5fde1ab2c2590bfc4f0b8868f0b946b101af053c
Allan <nabuleallan1@gmail.com>
2025-07-23 11:26:28 +0300

    updated paginator

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,7 +1,15 @@
 from django import pagination
+from requests import Response
 from rest_framework.pagination import PageNumberPagination
 
 class MessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,  # total items
+            'num_pages': self.page.paginator.num_pages,
+            'results': data
+        })
