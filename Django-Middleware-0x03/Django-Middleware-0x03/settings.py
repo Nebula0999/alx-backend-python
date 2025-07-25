9de4f999335d9dcb1edf9c60041bcd672402556c
Allan <nabuleallan1@gmail.com>
2025-07-25 20:43:17 +0300

    added restricting middleware

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',  # Custom middleware to restrict access by time
 
 ]
 
