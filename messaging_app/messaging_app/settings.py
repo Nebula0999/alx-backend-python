fa69150295333e723f770d28d568295408fd9bac
Allan <nabuleallan1@gmail.com>
2025-08-09 09:52:21 +0300

    created .env and docker-compose

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -154,3 +154,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+'''
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+    }
+}
+'''
